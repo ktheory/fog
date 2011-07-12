f897991f1d9c92f42f625abe645bf6d0b8a2bda1
Aaron Suggs <aaron@ktheory.com>
2011-07-12 18:45:12 -0400

    [aws|acs] Start of Amazon Cache Service
    Adds ACS#create_cache_security_group

--- lib/fog/providers/aws.rb
+++ lib/fog/providers/aws.rb
@@ -7,6 +7,7 @@ module Fog
 
     extend Fog::Provider
 
+    service(:acs,             'aws/acs')
     service(:auto_scaling,    'aws/auto_scaling')
     service(:cdn,             'cdn/aws')
     service(:compute,         'compute/aws')
