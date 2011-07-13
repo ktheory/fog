f154828ec859279bfc96f6117dbce3f0f1d3b67b
Aaron Suggs <aaron@ktheory.com>
2011-07-13 16:14:25 -0400

    [aws|acs] Added security group methods
    ACS#authorize_cache_security_group
    ACS#revoke_cache_security_group
    ACS#describe_cache_security_groups

--- lib/fog/aws/parsers/acs/describe_security_groups.rb
+++ lib/fog/aws/parsers/acs/describe_security_groups.rb
@@ -2,24 +2,25 @@ module Fog
   module Parsers
     module AWS
       module ACS
-
         require 'fog/aws/parsers/acs/security_group_parser'
 
-        class CreateCacheSecurityGroup < Fog::Parsers::AWS::ACS::SecurityGroupParser
+        class DescribeSecurityGroups < SecurityGroupParser
+
+          def reset
+            super
+            @response['CacheSecurityGroups'] = []
+          end
 
           def end_element(name)
             case name
-            when 'CacheSecurityGroup' then
-              @response['CacheSecurityGroup'] = @security_group
+            when 'CacheSecurityGroup'
+              @response["#{name}s"] << @security_group
               reset_security_group
             else
               super
             end
-
           end
-
         end
-
       end
     end
   end
